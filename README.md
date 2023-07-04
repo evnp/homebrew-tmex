a5e0cb72a462625cc7552c6095ffc69108746607
Evan Purcer <ep@e2.gg>
2023-07-04 10:39:02 -0700

    initial

---
+++ README.md
@@ -0,0 +1,33 @@
+Homebrew tap for
+
+```
+ ______   __    __    ______    __  __
+/\__  _\ /\ \  |\ \  /\  ___\  /\_\_\_\
+\/_/\ \/ \ \ \ | \ \ \ \  ___\ \/_/\_\/_
+   \ \_\  \ \_\ \ \_\ \ \_____\  /\_\/\_\
+    \/_/   \/_/  \/_/  \/_____/  \/_/\/_/  tmux execute
+```
+
+A lightweight tmux command & layout composer - one shell script + tmux + zero other dependencies
+
+Please see https://github.com/evnp/tmex for full documentation.
+
+Install
+-------
+```sh
+brew tap evnp/tmex
+brew install tmex
+```
+Verify installation:
+```sh
+tmex -v
+==> tmex 2.0.0-rc.3
+brew test tmex
+==> Testing tmex
+==> /opt/homebrew/Cellar/tmex/2.0.0-rc.3/bin/tmex test --print 1234 hello world
+```
+
+License
+-------
+MIT
+
---
+++ tmex.rb
@@ -0,0 +1,23 @@
+class Tmex < Formula
+  desc "tmux-execute – a lightweight tmux command & layout composer"
+  homepage "https://github.com/evnp/tmex"
+  url "https://github.com/evnp/tmex/archive/v2.0.0-rc.3.tar.gz"
+  sha256 "47a7342e2dfd893d1fb3ee073ebf1cf5b67e92102d5fb034dfe10a87cc21a5d9"
+  license "MIT"
+
+  def install
+    man1.install "man/tmex.1"
+    bin.install "tmex"
+  end
+
+  test do
+    assert_equal shell_output("#{bin}/tmex test --print 1234 hello world").chomp,
+      "new-session -s test ; send-keys hello Enter ; split-window -h -l50% ; "\
+      "select-pane -L ; split-window -h -l50% ; send-keys world Enter ; "\
+      "select-pane -R ; split-window -h -l50% ; select-pane -L ; select-pane -L ; "\
+      "select-pane -L ; select-pane -R ; split-window -v -l50% ; select-pane -R ; "\
+      "split-window -v -l67% ; split-window -v -l50% ; select-pane -R ; "\
+      "split-window -v -l50% ; select-pane -U ; split-window -v -l50% ; "\
+      "select-pane -D ; split-window -v -l50% "
+  end
+end
